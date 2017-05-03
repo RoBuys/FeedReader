3ece65c9ff183a4c918cb1853d426f6fb2be9dd5
RoBuys <robert@buys.fr>
2017-05-03 14:15:15 +0200

    Add files via upload
    Add files for Feedly integration

---
+++ RSSharp/Feedly/Model/Subscription.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSSharp.Feedly.Model
+{
+    public class Subscription
+    {
+        public string id { get; set; }
+        public string title { get; set; }
+        public List<Category> categories { get; set; }
+        public string sortid { get; set; }
+        public long updated { get; set; }
+        public string website { get; set; }
+
+        public override string ToString()
+        {
+            if (title != null)
+            {
+                return "Subscription: " + title;
+            }
+            else
+            {
+                return base.ToString();
+            }
+        }
+    }
+}
---
+++ RSSharp/Feedly/Model/Tag.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSSharp.Feedly.Model
+{
+    public class Tag
+    {
+        public string id { get; set; }
+        public string label { get; set; }
+
+        public static string get_tag_id_for_label(string label, string user_id)
+        {
+            return string.Format("user/{0}/tag/{1}", System.Web.HttpUtility.UrlEncode(user_id),System.Web.HttpUtility.UrlEncode(label));
+        }
+    }
+}
